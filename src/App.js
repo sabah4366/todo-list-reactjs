60d9ce795e69d9316d45ad06ea3f6d451dacab24
sabah <mhdsabah42@gmail.com>
2024-01-31 22:59:29 +0530

    todo list

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Todo from './Todo'
+function App() {
+
+ 
+  
+
+  return (
+   <>
+   <Todo />
+   </>
+
+  );
+}
+
+
+export default App;
+// this exporting is for other file can use this component
